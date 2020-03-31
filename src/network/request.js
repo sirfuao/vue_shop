eab103c3118fa4194c05e629e0d07e6e58b9133a
sirfuao <664431710@qq.com>
2020-03-31 10:05:44 +0800

    接口修改

--- src/network/request.js
+++ src/network/request.js
@@ -4,9 +4,7 @@ import axios from "axios";
 export function request(config) {
     // 1、创建 axios 实例
     const instance = axios.create({
-        // baseURL:'http://152.136.185.210.8000/api/h3',
-        baseURL:'http://123.207.32.32:8000/api/h3',
-        // baseURL:'http://106.54.54.237:8000/api/h3',
+        baseURL:'http://localhost/abc',
         timeout:5000
     });
     // 2、axios 的拦截
